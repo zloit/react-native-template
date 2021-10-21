dfcb80169cb71666c739c1ecd99662d89fb2b49b
Тимур Злобин <tzlobin@zennex.ru>
2021-10-21 18:06:08 +0700

    build: add, configure eslint-plugin-simple-import-sort

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,4 +1,35 @@
+const fs = require('fs');
+
+const folders = fs
+  .readdirSync('src', {withFileTypes: true})
+  .filter(dirent => dirent.isDirectory())
+  .map(dirent => '@' + dirent.name);
+
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  plugins: ['simple-import-sort'],
+  rules: {
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+  },
+  overrides: [
+    {
+      files: ['*.tsx', '*.ts'],
+      rules: {
+        'simple-import-sort/imports': [
+          'error',
+          {
+            groups: [
+              ['^\\u0000'],
+              ['^react', '^@?\\w'],
+              [`^(${folders.join('|')})(/.*|$)`, '^\\.'],
+              ['^'],
+              ['^\\.'],
+            ],
+          },
+        ],
+      },
+    },
+  ],
 };
--- template/index.ts
+++ template/index.ts
@@ -3,6 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
+
 import App from '@app/index';
 import {name as appName} from './app.json';
 
