dcc4f03273b94aec2bd1253a86d713f1a9b12dde
Тимур Злобин <tzlobin@zennex.ru>
2021-10-26 23:32:30 +0700

    build(boundaries): add, configure

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -7,11 +7,103 @@ const folders = fs
 
 module.exports = {
   root: true,
-  extends: '@react-native-community',
-  plugins: ['simple-import-sort'],
+  extends: ['@react-native-community', 'plugin:boundaries/recommended'],
+  plugins: ['simple-import-sort', 'boundaries'],
   rules: {
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
+    'boundaries/element-types': [
+      2,
+      {
+        default: 'allow',
+        rules: [
+          {
+            from: 'shared',
+            disallow: ['entities', 'features', 'pages', 'processes', 'app'],
+          },
+          {
+            from: 'entities',
+            disallow: ['features', 'pages', 'processes', 'app'],
+          },
+          {
+            from: 'features',
+            disallow: ['pages', 'app'],
+          },
+          {
+            from: 'pages',
+            disallow: ['app'],
+          },
+        ],
+      },
+    ],
+    'boundaries/entry-point': [
+      2,
+      {
+        // disallow all entry-points by default
+        default: 'disallow',
+        rules: [
+          {
+            // when importing any modules
+            target: [
+              'shared',
+              'entities',
+              'features',
+              'pages',
+              'processes',
+              'app',
+            ],
+            // only allow index.js
+            allow: 'index.{ts,tsx}',
+          },
+        ],
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+    'boundaries/elements': [
+      {
+        type: 'pages',
+        pattern: 'pages',
+        mode: 'folder',
+        capture: ['stack', 'page'],
+      },
+      {
+        type: 'app',
+        pattern: 'app',
+        mode: 'folder',
+        capture: ['slice', 'segment'],
+      },
+      {
+        type: 'entities',
+        pattern: 'entities',
+        mode: 'folder',
+        capture: ['slice', 'segment'],
+      },
+      {
+        type: 'features',
+        pattern: 'features',
+        mode: 'folder',
+        capture: ['slice', 'segment'],
+      },
+      {
+        type: 'shared',
+        pattern: 'shared',
+        mode: 'folder',
+        capture: ['slice', 'segment'],
+      },
+      {
+        type: 'processes',
+        pattern: 'processes',
+        mode: 'folder',
+        capture: ['slice', 'segment'],
+      },
+    ],
+    'boundaries/ignore': ['.eslintrc.js'],
   },
   overrides: [
     {
--- template/__tests__/App-test.tsx
+++ template/__tests__/App-test.tsx
@@ -8,7 +8,7 @@ import React from 'react';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-import App from '@app/index';
+import App from 'app';
 
 it('renders correctly', () => {
   renderer.create(<App />);
